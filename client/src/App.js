db19afd8359cf020ad1d84baf05162a244db0e99
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-14 13:04:13 +0900

    feat: Create From and Post component
    - Create GlobalStyle.js to apply the styles globally

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import GlobalStyles from './GlobalStyles';
+
+const Container = styled.div`
+    border: 1px solid black;
+    background-color: blue;
+`;
+
+const Header = styled.header`
+    border: 1px solid black;
+    background-color: yellow;
+`;
+
+const Logo = styled.img`
+    border: 1px solid black;
+    background-color: green;
+`;
+
+const Title = styled.h2`
+    border: 1px solid black;
+    background-color: pink;
+    text-align: center;
+`;
+
+const ContentsContainer = styled.main`
+    display: flex;
+    border: 1px solid black;
+    background-color: mintcream;
+    height: 100vh;
+`;
+
+const PostsContainer = styled.aside`
+    width: 50vw;
+    border: 1px solid black;
+    background-color: coral;
+
+`;
+
+const FormContainer = styled.div`
+    width: 50vw;
+    border: 1px solid black;
+    background-color: lightcyan;
+`;
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <>
+            <Container>
+                <Header>
+                    <Logo src="" alt=""/>
+                    <Title>Good memories</Title>
+                </Header>
+                <ContentsContainer>
+                    <PostsContainer>
+                        PostContainer
+                        <Posts />
+                    </PostsContainer>
+                    <FormContainer>
+                        FormContainer
+                        <Form />
+                    </FormContainer>
+                </ContentsContainer>
+            </Container>
+            <GlobalStyles />
+        </>
     );
 };
 
